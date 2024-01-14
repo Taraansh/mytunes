5656f24a123af18e30b3ea76db4ea594bf37efb9
Taraansh <taraanshnandwani123@gmail.com>
2024-01-14 13:55:54 +0530

    Registered

--- tuneTracker/admin.py
+++ tuneTracker/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from tuneTracker.models import Artist, Song
+from tuneTracker.models import Artist, Song, Contact
 
 # Register your models here.
 admin.site.register(Artist)
 admin.site.register(Song)
+admin.site.register(Contact)
